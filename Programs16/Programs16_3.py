3a69cc0e80cbc1101cb587600cc84a55b6dc1e2b
Siddhi Kulkarni <siddhikulkarni200@gmail.com>
2026-06-15 17:24:34 +0530

    Add files via upload

---
+++ Programs16/Programs16_3.py
@@ -0,0 +1,27 @@
+def Pattern(row,col):
+
+    for i in range(1,row + 1):
+        for j in range(col,0,-1):
+
+            if(i == j):
+                print("$",end = "\t")
+
+            elif(i > j):
+                print("*",end = "\t")
+
+            else:
+                print("#",end = "\t")
+
+        print("\n")
+
+def main():
+    print("Enter the number of rows: ")
+    r = int(input())
+
+    print("Enter the number of columns: ")
+    c = int(input())
+
+    Pattern(r,c)
+
+if __name__ == "__main__":
+    main()
